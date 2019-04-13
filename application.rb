3072005d2373af7b6c93db567baef968ed29e3da
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-13 21:44:05 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World my name is Johnson Joseph"
+    resp.write "Hello, World my name is"
     resp.finish
   end
 
